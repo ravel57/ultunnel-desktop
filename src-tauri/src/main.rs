a04f977ccf7d83ce6c98cd7d15143d0c54805962
petr <petyalom@yandex.ru>
2026-02-02 21:13:11 +0300

    added starting as admin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    ultunnel_desktop_lib::run()
+    #[cfg(target_os = "windows")]
+    {
+        let mut windows = tauri_build::WindowsAttributes::new();
+        windows = windows.app_manifest(include_str!("manifest.xml").to_string());
+        tauri_build::try_build(tauri_build::Attributes::new().windows_attributes(windows))
+            .expect("tauri build failed");
+    }
+    #[cfg(target_os = "macos")]
+    {
+        ultunnel_desktop_lib::run()
+    }
 }
